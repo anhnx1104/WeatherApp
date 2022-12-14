06d2e24c53bbadb9c993ca73a18a1b75f060d523
ANHNX4 <anh833905@gmail.com>
2022-12-14 13:35:58 +0700

    tìm kiếm bằng giọng nói

--- js/index.js
+++ js/index.js
@@ -36,3 +36,59 @@ searchInput.addEventListener("change", (e) => {
     windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
   });
 });
+
+// tim kiem giong noi
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = "vi-VI";
+recognition.continuous = false;
+
+const microphone = document.querySelector(".microphone");
+
+const handleVoice = (text) => {
+  console.log(text);
+
+  const handleText = text.toLowerCase();
+  if (handleText.includes("thời tiết tại")) {
+    const location = handleText.split("tại")[1].trim();
+    searchInput.value = location;
+    changeEvent = new Event("change");
+    searchInput.dispatchEvent(changeEvent);
+    return;
+  }
+
+  const container = document.querySelector(".container");
+  if (handleText.includes("thay đổi màu nền")) {
+    const color = handleText.split("màu nền")[1].trim();
+    container.style.background = color;
+    return;
+  }
+  if (handleText.includes("màu nền mặc định")) {
+    container.style.background = "";
+
+    return;
+  }
+};
+
+microphone.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  recognition.start();
+});
+
+recognition.onspeechend = () => {
+  recognition.stop();
+};
+
+recognition.onerror = (e) => {
+  console.log(e);
+};
+
+recognition.onresulte = (e) => {
+  console.log(e);
+  const text = e.result[0][0].transcript;
+  handleVoice(text);
+};
